0360b941f4fadf4b88a8a5145809675b863357f7
MernaWessa <mirnawessa@gmail.com>
2024-08-19 23:55:00 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,32 @@
+num1= float(input("put the first num:"))
+num2= float(input("put the second num:"))
+operator= input("put the operator")
+if (operator== "+" ):
+    print (num1+num2)
+if(operator=="-"):
+    print(num1-num2)
+if(operator=="*"):
+    print(num1*num2)
+if(operator=="/"):
+    print(num1/num2)
+if(operator=="%"):
+    print(num1%num2)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
